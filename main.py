31c7ae9dc88d621f6f31c9a91d0ec9ae8247b362
Eraserman360 <shiverskij.georgij@mail.ru>
2023-03-31 16:37:12 +0500

    Add new regression methods

--- main.py
+++ main.py
@@ -5,24 +5,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-data_path = "D:\Machine_learning\Data_sets\dataSet_Well5_GHE.csv"
+data_path = "D:\Machine_learning\Data_sets\DataWithoutNaN_columns.csv"
 well_data = pd.read_csv(data_path, sep = ",")
 # well_data.info()
-well_data = well_data.dropna()
-# well_data.info()
-feauters_start = ['AFRT', 'AF90', 'AF30', 'AF60', 'AF20', 'AF10', 'AMF', 'BIT', 'CFTC',
-       'CNTC', 'GR', 'HCAL', 'HDRA', 'HMIN', 'HMNO', 'HTEM', 'PEFZ', 'RHOZ',
-       'RLA1', 'RLA2', 'RLA3', 'RLA4', 'RLA5', 'RT_HRLT', 'RXOZ', 'SP', 'KPA',
-       'DOLM', 'KPob', 'KPD', 'KPkv', 'KPmz', 'KPN', 'KSDR', 'KTIM', 'LIME',
-       'PORN', 'PORW', 'SHALE', 'TCMR', 'CMFF', 'SWE']
-
+# well_data = well_data.dropna(axis = 1)
+print(well_data.columns)
+feauters_start = ['AF90', 'AF30', 'AF60', 'GR', 'HCAL', 'HTEM', 'PEFZ', 'RHOZ', 'RLA3',
+       'RLA4', 'SHALE']
+# # well_data.to_excel("DataWithoutNaN_columns.xlsx")
 
 
 def create_data_set(data, feauters):
     from sklearn.model_selection import train_test_split
-    y = data.GHE
+    y = data.poro
     X = data[feauters]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state= 1, test_size=0.25)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state= 1, test_size=0.2)
     return X_train, X_test, y_train, y_test
 
 
@@ -46,20 +43,38 @@ def choose_parametrs(model, index_arrey):
     new_feature = []
     for feature in feature_dict:
         importenses = feature_dict[feature]
-        if importenses >= 0.02:
-            print("Добавлен: ", feature)
+        if importenses >= 0.01:
+            # print("Добавлен: ", feature)
             new_feature.append(feature)
     print(new_feature)
     return new_feature
 
 
+def lin_regr(X_train, X_test, y_train, y_test):
+    from sklearn.linear_model import LinearRegression
+    lin_regres_model = LinearRegression()
+    lin_regres_model.fit(X_test,y_test)
+    lin_regres_prediction = lin_regres_model.predict(X_test)
+    #ЛАССО
+    from sklearn.linear_model import Lasso
+    lasso_model = Lasso(alpha=1)
+    lasso_model.fit(X_test,y_test)
+    lasso_prediction = lasso_model.predict(X_test)
+    return lin_regres_prediction, lasso_prediction
 
 def RFR(X_train, X_test, y_train, y_test):
     from sklearn.ensemble import RandomForestRegressor
-    rf_model = RandomForestRegressor(criterion= 'squared_error', random_state=1)
-    rf_model.fit(X_train, y_train)
-    rf_fzi_predictions = rf_model.predict(X_test)
-    return rfr_model, rfr_fzi_predictions
+    rfr_model = RandomForestRegressor(criterion= 'squared_error', random_state=1)
+    rfr_model.fit(X_train, y_train)
+    rfr_predictions = rfr_model.predict(X_test)
+    return rfr_model, rfr_predictions
+
+def Gr_boost(X_train, X_test, y_train, y_test):
+    from sklearn.ensemble import GradientBoostingRegressor
+    grb_model = GradientBoostingRegressor(random_state=1)
+    grb_model.fit(X_train,y_train)
+    grb_prediction = grb_model.predict(X_test)
+    return grb_model, grb_prediction
 
 
 def RFC(X_train, X_test, y_train, y_test):
@@ -84,13 +99,10 @@ def quality_of_regression_model(method, prediction, y_test):
     from sklearn.metrics import r2_score
     from sklearn.metrics import mean_squared_error
     from sklearn.metrics import mean_absolute_percentage_error
-    print("y_test: ", "\n", y_test)
-    print(type(prediction))
-    print("prediction: \n",)
     arrey_to_list = prediction.tolist()
     count = 0
     for i in arrey_to_list:
-        print(arrey_to_list[count])
+        # print(arrey_to_list[count])
         count += 1
     mae = mean_absolute_error(y_test, prediction)
     determination = r2_score(y_test, prediction)
@@ -102,18 +114,44 @@ def quality_of_regression_model(method, prediction, y_test):
     print('MSE', method, ': ', MSE)
     return
 
-X_train, X_test, y_train, y_test = create_data_set(well_data,feauters_start)
 
-rfc_model, GHE_pred = RFC(X_train, X_test, y_train, y_test)
-new_feature = choose_parametrs(rfc_model, feauters_start)
-X_train, X_test, y_train, y_test = create_data_set(well_data, new_feature)
-rfc_model, GHE_pred = RFC(X_train, X_test, y_train, y_test)
-quality_of_classification_model("RandomForestClassifier", y_test, GHE_pred)
 
-# rf_model, rf_fzi = RFR(X_train, X_test, y_train, y_test)
-# new_feature = choose_parametrs(rf_model, feauters_start)
+# Классификация - на потом.
+# X_train, X_test, y_train, y_test = create_data_set(well_data,feauters_start)
+#
+# rfc_model, GHE_pred = RFC(X_train, X_test, y_train, y_test)
+# new_feature = choose_parametrs(rfc_model, feauters_start)
 # X_train, X_test, y_train, y_test = create_data_set(well_data, new_feature)
-# rf_model, rf_fzi = RFR(X_train, X_test, y_train, y_test)
+# rfc_model, GHE_pred = RFC(X_train, X_test, y_train, y_test)
+# quality_of_classification_model("RandomForestClassifier", y_test, GHE_pred)
+
+def Line_regression_models():
+    X_train, X_test, y_train, y_test = create_data_set(well_data, feauters_start)
+    lin_regres_prediction, lasso_prediction =lin_regr(X_train, X_test, y_train, y_test)
+    quality_of_regression_model("LinearRegression", lin_regres_prediction, y_test)
+    quality_of_regression_model("LassoRegression", lasso_prediction, y_test)
+    return
 
 
+def RandomForrest():
+    X_train, X_test, y_train, y_test = create_data_set(well_data, feauters_start)
+    rf_model, rf_fzi = RFR(X_train, X_test, y_train, y_test)
+    new_feature = choose_parametrs(rf_model, feauters_start)
+    X_train, X_test, y_train, y_test = create_data_set(well_data, new_feature)
+    rf_model, rf_fzi = RFR(X_train, X_test, y_train, y_test)
+    quality_of_regression_model("RandomForestRegression", rf_fzi, y_test)
+    return
 
+
+def GradientBoost():
+    X_train, X_test, y_train, y_test = create_data_set(well_data, feauters_start)
+    grb_model, grb_prediction = Gr_boost(X_train, X_test, y_train, y_test)
+    new_feature = choose_parametrs(grb_model, feauters_start)
+    X_train, X_test, y_train, y_test = create_data_set(well_data, new_feature)
+    grb_model, grb_prediction = Gr_boost(X_train, X_test, y_train, y_test)
+    quality_of_regression_model("GradientBoostingRegressor", grb_prediction, y_test)
+    return
+
+Line_regression_models()
+RandomForrest()
+GradientBoost()
