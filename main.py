cfd98ff39619224edacd9a192175db0d69c67536
Eraserman360 <shiverskij.georgij@mail.ru>
2024-05-14 09:30:23 +0500

    export predicted porosity

--- main.py
+++ main.py
@@ -5,26 +5,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-data_path = "Data_sets/ready/new_db_kgis.xlsx"
+data_path = "Data_sets/ready/db_variation.xlsx"
 well_data = pd.read_excel(data_path)
 well_data.info()
-# well_data = well_data.dropna(axis='columns', thresh=(380*0.85))
+well_data = well_data.dropna(axis='columns', thresh=(889*0.85))
 well_data = well_data.dropna(axis='index')
 print(well_data.columns)
 
-feauters_start = ['MPZ', 'MPZ', 'MGZ', 'A04M01N', 'A1M01N',
-       'A2M05N', 'A4M05N', 'A8M1N', 'BK', 'BMK', 'GGKP', 'IK', 'NKTB', 'W',
-       'GK', 'DS', 'DS_M', 'MGZ-MPZ']
+feauters_start = ['A04M01N', 'A1M01N', 'A2M05N', 'BK', 'IK', 'NKTB', 'W', 'GK']
 target = 'Пористость в атм, %'
+well_data = well_data[[target, 'A04M01N', 'A1M01N', 'A2M05N', 'BK', 'IK', 'NKTB', 'W', 'GK']]
 #well_data = well_data.drop(['Месторождение', 'Скважина', 'Пласт', 'Глубина(привзяка). м','DEPT'], axis=1)
 correlation_matrix = well_data.corr(numeric_only=True)
 plt.figure(figsize= (20, 15))
 sns.heatmap(correlation_matrix, annot = True, annot_kws={"size":7}, vmin=-1, vmax=1, center=0, cmap ='RdYlGn')
 plt.show()
+gis_data = pd.read_csv('gis_data/v_terr.csv', sep='*')
+gis_data.info()
+gis_data.set_index(['DEPT', 'NSKV'], inplace=True)
+gis_test = gis_data[['A04M01N', 'A1M01N', 'BK', 'IK', 'NKTB', 'W', 'GK']]
+gis_test = gis_test.dropna(axis='index')
+
 
 def choose_feauters_by_corelation(features, target):
     correlation_with_target = well_data.corr(numeric_only=True)[target].drop(target)
-    threshold = 0.1
+    threshold = 0.05
     high_correlation_features = correlation_with_target[abs(correlation_with_target) > threshold].index.tolist()
     return high_correlation_features
 
@@ -101,12 +106,11 @@ def RFR(X_train, X_test, y_train, y_test):
     rfr_model = RandomForestRegressor()
     rfr_model.fit(X_train, y_train)
     rfr_predictions = rfr_model.predict(X_test)
-    parametrs = {'n_estimators': [310],
-                'max_depth': [25,30,35],
-                 'random_state': [20,25],
-                 'max_leaf_nodes': [60,70,80],
-                 'min_samples_leaf': [1]
-               }
+    parametrs = {   'n_estimators': [50, 100, 150],
+                    'max_depth': [None, 5, 10],
+                    'min_samples_split': [2, 5, 10],
+                    'min_samples_leaf': [1, 2, 4]
+                 }
     rs = GridSearchCV(rfr_model,
                       parametrs,
                       cv=5,
@@ -123,18 +127,6 @@ def Gr_boost(X_train, X_test, y_train, y_test):
     grb_prediction = grb_model.predict(X_test)
     np.savetxt('grb_prediction', grb_prediction, fmt='%.2f', delimiter=' ')
     return grb_model, grb_prediction
-
-
-def RFC(X_train, X_test, y_train, y_test):
-    from sklearn.ensemble import RandomForestClassifier
-    #params = {'max_depth': 11, 'min_samples_leaf': 7, 'min_samples_split': 6, 'n_estimators': 40}
-    rfc_model = RandomForestClassifier(n_estimators=40,
-                                       max_depth=11,
-                                       min_samples_leaf=7,
-                                       min_samples_split=6)
-    rfc_model.fit(X_train, y_train)
-    GHE_pred = rfc_model.predict(X_test)
-    return rfc_model, GHE_pred
 
 
 def quality_of_classification_model (method, y_test, y_pred):
@@ -166,24 +158,6 @@ def quality_of_regression_model(method, prediction, y_test):
     print('MSE', method, ': ', MSE)
     return
 
-
-
-# Классификация - на потом.
-# from sklearn.model_selection import GridSearchCV
-# X_train, X_test, y_train, y_test = create_data_set(well_data,feauters_start)
-# rfc_model, GHE_pred = RFC(X_train, X_test, y_train, y_test)
-# new_feature = choose_parametrs(rfc_model, feauters_start)
-# X_train, X_test, y_train, y_test = create_data_set(well_data, new_feature)
-# rfc_model, GHE_pred = RFC(X_train, X_test, y_train, y_test)
-# parametrs = { 'n_estimators': range (10, 51, 10),
-#               'max_depth': range (1,13, 2),
-#               'min_samples_leaf': range (1,8),
-#               'min_samples_split': range (2,10,2) }
-# grid = GridSearchCV(rfc_model, parametrs, cv=5)
-# grid.fit(X_train, y_train)
-# print(grid.best_params_)
-#quality_of_classification_model("RandomForestClassifier", y_test, GHE_pred)
-
 def Line_regression_models():
     X_train, X_test, y_train, y_test = create_data_set(well_data, feauters_start)
     lin_regres_prediction, lasso_prediction =lin_regr(X_train, X_test, y_train, y_test)
@@ -199,6 +173,23 @@ def RandomForrest():
     X_train, X_test, y_train, y_test = create_data_set(well_data, new_feature)
     rf_model, rf_fzi = RFR(X_train, X_test, y_train, y_test)
     quality_of_regression_model("RandomForestRegression", rf_fzi, y_test)
+    x = well_data[new_feature]
+    x = scaling_data(x)
+    rf_model.fit(x, well_data[target])
+    gis_d = gis_test[new_feature]
+    gis_scaled = scaling_data(gis_d)
+    predictet_poro = rf_model.predict(gis_scaled)
+    gis_out = gis_d
+    gis_out['poro'] = predictet_poro
+
+    hist, bins = np.histogram(predictet_poro, bins=10, density=True)
+    percentages = (hist / np.sum(hist)) * 100
+    plt.bar(bins[:-1], percentages, width=np.diff(bins))
+    plt.xlabel('Value')
+    plt.ylabel('Percentage')
+    plt.title('Percentage Histogram')
+    plt.show()
+    gis_out.to_csv('gis_data/v_terr_predicted_2.prn', sep='*')
     return
 
 
